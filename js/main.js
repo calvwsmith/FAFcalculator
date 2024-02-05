4b4db46933a8a89f362e2e49d459799da9667157
Calvin <calsmith@iastate.edu>
2024-02-04 18:07:44 -0600

    added pages

--- js/main.js
+++ js/main.js
@@ -9,3 +9,79 @@ function myFunction() {
         ele.style.display = "none";
     }
 }
+db.style.display = "block";
+sim.style.display = "none";
+acu.style.display = "none";
+function simSwitch() {
+    var db = document.getElementById("db");
+    var sim = document.getElementById("sim");
+    var acu = document.getElementById("acu");
+    db.style.display = "none";
+    sim.style.display = "block";
+    acu.style.display = "none";
+}
+function dbSwitch() {
+    var db = document.getElementById("db");
+    var sim = document.getElementById("sim");
+    var acu = document.getElementById("acu");
+    db.style.display = "block";
+    sim.style.display = "none";
+    acu.style.display = "none";
+}
+function acuSwitch() {
+    var db = document.getElementById("db");
+    var sim = document.getElementById("sim");
+    var acu = document.getElementById("acu");
+    db.style.display = "none";
+    sim.style.display = "none";
+    acu.style.display = "block";
+}
+titan = new Unit(weapons = [new weapon(damage = 5, ROF = 5)]);
+document.getElementById("myPara").innerHTML = "titan.getDps()";
+
+class Unit {
+    constructor(name, tier, operation, health, armor, shield, shieldRegen, regen, massCost, energyCost, eYield, mYield, buildTime, speed, vision, radar, weapons, abilities, sonar, omni, waterVision, turnRate){
+        this.name = name;
+        this.tier = tier;
+        this.operation = operation;
+        this.health = health;
+        this.armor = armor;
+        this.shield = shield;
+        this.shieldRegen = shieldRegen;
+        this.regen = regen;
+        this.massCost = massCost;
+        this.energyCost = energyCost;
+        this.eYield = eYield;
+        this.mYield = mYield;
+        this.buildTime = buildTime;
+        this.speed = speed;
+        this.vision = vision;
+        this.radar = radar;
+        this.weapons = weapons;
+        this.abilities = abilities;
+        this.sonar = sonar;
+        this.omni = omni;
+        this.waterVision = waterVision;
+        this.turnRate = turnRate;
+    }
+    getDps(){
+        let dps = 0;
+        for (x in weapons){
+            dps += x.damage * x.ROF;
+        }
+        return dps;
+    }
+}
+
+class weapon {
+    constructor(rangeMin, rangeMax, damage, operation, ROF, name, tPri){
+        this.rangeMin = rangeMin;
+        this.rangeMax = rangeMax;
+        this.damage = damage;
+        this.operation = operation;
+        this.ROF = ROF;
+        this.name = name;
+        this.tPri = tPri;
+
+    }
+}
